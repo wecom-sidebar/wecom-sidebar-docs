ea814cf6f62768bff5c8aa8e8356278e212583d7
Haixiang <haixiang6123@gmail.com>
2021-03-11 01:09:14 +0800

    feat: 添加起步文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
   title: '企微侧栏权威开发指南',
-  description: '企业微信侧边栏开发方案、教程、FAQ'
+  description: '企业微信侧边栏开发方案、教程、FAQ',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '介绍',
+        children: []
+      },
+      {
+        title: '指南',
+        children: [
+          '/guide/prepare',
+          '/guide/config_sidebar_h5'
+        ]
+      },
+      {
+        title: 'FAQ',
+        children: []
+      }
+    ]
+  }
 }
