f10df186a26d13d895f1bb8759293e8705f4c23a
Haixiang <haixiang6123@gmail.com>
2021-03-11 03:41:19 +0800

    feat: 添加 logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,16 @@ module.exports = {
   title: '企微侧栏权威开发指南',
   description: '企业微信侧边栏开发方案、教程、FAQ',
   themeConfig: {
+    logo: '/images/logo.png',
     sidebar: [
       {
         title: '介绍',
+        collapsable: false,
         children: []
       },
       {
         title: '指南',
+        collapsable: false,
         children: [
           '/guide/prepare',
           '/guide/config_sidebar_h5'
@@ -16,6 +19,7 @@ module.exports = {
       },
       {
         title: 'FAQ',
+        collapsable: false,
         children: []
       }
     ]
