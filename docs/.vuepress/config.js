6bd799d7d23e177ceaaa44cd3867a4a6376fd7a0
Haixiang <haixiang6123@gmail.com>
2021-03-15 00:06:35 +0800

    feat: 去掉关于

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,6 @@ module.exports = {
         title: '方案',
         collapsable: false,
         children: [
-          '/solutions/about',
           '/solutions/cache',
           '/solutions/user_auth',
           '/solutions/get_jsapi_ticket',
@@ -39,7 +38,6 @@ module.exports = {
         title: 'FAQ',
         collapsable: false,
         children: [
-          '/faq/about',
           '/faq/has_jssdk_npm',
           '/faq/agentConfig_is_not_a_function',
           '/faq/can_not_open_debug',
